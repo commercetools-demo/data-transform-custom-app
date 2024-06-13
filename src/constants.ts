cf7d9408e242404045c92eb9b9bbef307fddf4c7
Behnam Tehrani <behnam777@gmail.com>
2024-06-13 11:46:33 -0400

    rename entrypoint

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 // Make sure to import the helper functions from the `ssr` entry point.
 import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr';
 
-export const entryPointUriPath = 'overview';
+export const entryPointUriPath = 'data-transform-overview';
 
 export const PERMISSIONS = entryPointUriPathToPermissionKeys(entryPointUriPath);
 
